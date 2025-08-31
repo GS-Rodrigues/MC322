7047eac24959d7fed04d00e5d8e2038b9a77ac00
Guilherme da Silva Rodrigues <168394483+GS-Rodrigues@users.noreply.github.com>
2025-08-31 19:47:32 -0300

    Update Corrupted_IA.java

--- tarefa1/src/Corrupted_IA.java
+++ tarefa1/src/Corrupted_IA.java
@@ -1,8 +1,26 @@
+import java.util.Random;
+
 public class Corrupted_IA extends Monstro 
 {
+    private static final String[] ataques = 
+    {
+        "lança um ataque de código corrompido!",
+        "sobrecarrega o sistema com loops infinitos!",
+        "injeta vírus digitais devastadores!",
+        "distorce a realidade virtual e causa instabilidade!",
+        "emite uma onda de dados corrompidos que frita circuitos!"
+    }
+    
     public Corrupted_IA(String nome, int pontosDeVida, int forca) 
     {
-        super(nome, pontosDeVida, forca, xpConcedido);
+        super(nome, pontosDeVida, forca, 100);
     }
 
+    @Override
+    protected void atacar(Personagem alvo) 
+    {
+        String ataque = ataques[random.nextInt(ataques.length)]
+        System.out.println(this.getNome() + " " + ataque;
+        alvo.receberDano(this.getForca());
+    }
 }
