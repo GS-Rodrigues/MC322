b671b5ae5ac1919df2044ca8c9e20b7dde8ed41e
Guilherme da Silva Rodrigues <168394483+GS-Rodrigues@users.noreply.github.com>
2025-08-31 19:33:10 -0300

    Update Corrupted_IA.java

--- tarefa1/src/Corrupted_IA.java
+++ tarefa1/src/Corrupted_IA.java
@@ -1,1 +1,8 @@
+public class Corrupted_IA extends Monstro 
+{
+    public Corrupted_IA(String nome, int pontosDeVida, int forca) 
+    {
+        super(nome, pontosDeVida, forca, xpConcedido);
+    }
 
+}
