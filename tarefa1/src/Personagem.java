30e28c357a1a702faf982045d8086e2b5f62c783
Giovanni Barbosa Herrero <giovanni.herrero22@gmail.com>
2025-08-31 17:21:46 -0300

    Adicionando a classe Personagem

---
+++ tarefa1/src/Personagem.java
@@ -0,0 +1,25 @@
+public abstract class Personagem {
+    String nome;
+    int pontosDeVida;
+    int forca;
+
+    public Personagem(String nome, int pontosDeVida, int forca) {
+        this.nome = nome;
+        this.pontosDeVida = pontosDeVida;
+        this.forca = forca;
+    }
+
+    public void receberDano(int dano) {
+        pontosDeVida = pontosDeVida - dano;
+        //Adicionar uma lógica para indicar que o personagem morreu seu pontos de vida<=0?
+    }
+
+    public void exibirStatus() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Vida: " + pontosDeVida);
+        System.out.println("Força: " + forca);
+    }
+
+    protected abstract void atacar(Personagem alvo);
+    
+}
