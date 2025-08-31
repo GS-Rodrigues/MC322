16cda31c09ac0ec2cbb09bbc5c02e3ec6f656fe9
Guilherme da Silva Rodrigues <168394483+GS-Rodrigues@users.noreply.github.com>
2025-08-31 19:23:55 -0300

    Update Monstro.java

--- tarefa1/src/Monstro.java
+++ tarefa1/src/Monstro.java
@@ -1,10 +1,12 @@
 public abstract class Monstro extends Personagem {
     public Monstro(String nome, int pontosDeVida, int forca) {
-        super(nome, pontosDeVida, forca)
+        super(nome, pontosDeVida, forca);
     }
 
     public int xpConcedido;
-    
+
+    @override
+    public void exibirStatus();
 
     }
 }
