87b9e60716ef8d33694336270f8b5be71c34e5cc
Guilherme da Silva Rodrigues <guilherme_silva_rodrigues@outlook.com.br>
2025-09-01 22:08:27 -0300

    Corrigido classe Corrupted IA

--- tarefa1/src/Corrupted_IA.java
+++ tarefa1/src/Corrupted_IA.java
@@ -9,18 +9,21 @@ public class Corrupted_IA extends Monstro
         "injeta vírus digitais devastadores!",
         "distorce a realidade virtual e causa instabilidade!",
         "emite uma onda de dados corrompidos que frita circuitos!"
-    }
+    };
+
+    Random gerador = new Random();
     
     public Corrupted_IA(String nome, int pontosDeVida, int forca) 
     {
         super(nome, pontosDeVida, forca, 100);
     }
-
+    
     @Override
     protected void atacar(Personagem alvo) 
     {
-        String ataque = ataques[random.nextInt(ataques.length)]
-        System.out.println(this.getNome() + " " + ataque;
+        String ataque = ataques[gerador.nextInt(ataques.length)];
+        System.out.println(this.getNome() + " " + ataque);
         alvo.receberDano(this.getForca());
     }
+
 }
---
+++ tarefa1/src/main.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public class main {
+    Random gerador = new Random();
+    //private static final String[] tipos_herois = {Corrupted_IA, };
+
+    //Class Heroi = tipos_herois[gerador.nextInt(tipos_herois.length)];
+
+    //private int heroi_escolhido = gerador.nextInt(2);
+    
+
+};
