6e0023d8e6717ca3261501c2c4fd67c9f6a8b078
Giovanni Barbosa Herrero <giovanni.herrero22@gmail.com>
2025-09-04 09:12:35 -0300

    Adicionando vetor de strings de ataques da classe Lehilton

--- tarefa1/src/Lehilton.java
+++ tarefa1/src/Lehilton.java
@@ -1,6 +1,19 @@
-public class Lehilton extends Heroi{
+public class Lehilton extends Heroi {
+
+    private String especialidade;
+
+    private static final String[] ataques =
+    {
+    "Lehilton aplica Bellman-Ford mentalmente, e encontra o caminho mais eficiente até o ponto fraco do inimigo!",
+    "O professor identifica o parâmetro exato que contra a complexidade do robô, e ataca com precisão milimétrica!",
+    "Lehilton aplicada milhares de cenários de falha no inimigo, até que revele seu bug fatal!",
+    "Lehilton gera uma Máquina de Estados Finitos e processa o inimigo como uma entrada inválida!",
+    "O professor toma a decisão localmente ótima rápidamente. O primeiro componente mais vulnerável do robô é destruído!"
+            };
+    
     public Lehilton(String nome, int vida, int ataque) 
     {
         super(nome, vida, ataque);
+        especialidade = "Pensamento combinatório";
     }
 }
