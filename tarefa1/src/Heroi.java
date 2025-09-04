d223e6127f08d01d339d5d0d3a57fb6b7dd07651
Giovanni Barbosa Herrero <giovanni.herrero22@gmail.com>
2025-09-04 08:07:02 -0300

    Adicionando atributos à classe Heroi

--- tarefa1/src/Heroi.java
+++ tarefa1/src/Heroi.java
@@ -1,5 +1,9 @@
 public class Heroi extends Personagem
 {
+
+    private int nivel;
+    private int experiencia;
+    
     public Heroi(String nome, int vida, int ataque) 
     {
         super(nome, vida, ataque);
