12b889dd904cf47cf0fe07444dba9cecc75f96f3
Giovanni Barbosa Herrero <giovanni.herrero22@gmail.com>
2025-09-04 08:12:28 -0300

    Adicionando método ganharExperiencia ao Heroi

--- tarefa1/src/Heroi.java
+++ tarefa1/src/Heroi.java
@@ -3,13 +3,20 @@ public class Heroi extends Personagem
 
     private int nivel;
     private int experiencia;
-    
+
     public Heroi(String nome, int vida, int ataque) 
     {
         super(nome, vida, ataque);
+        this.nivel = 1;
+        this.experiencia = 0;
     }
 
     public void atacar(Personagem inimigo) 
     {
     }
+
+    public void ganharExperiencia(int xpconcedido) {
+        this.experiencia += xpconcedido;
+    }
+    
 }
