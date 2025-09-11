1b845eb86586a6018a6140f943240468b8a126f4
Giovanni Barbosa Herrero <giovanni.herrero22@gmail.com>
2025-09-11 12:19:17 -0300

    Adicionando lista de armas à classe abstrata monstro

--- tarefa2/src/Monstro.java
+++ tarefa2/src/Monstro.java
@@ -1,7 +1,14 @@
 public abstract class Monstro extends Personagem 
 {
     private int xpConcedido;
-    
+
+ 
+    private static final Arma[] listaDeArmasParaLargar = 
+    {
+        new Codeblade(10, 0),
+        new Turing_Hammer(20, 1),
+        new Data_rifle(20,2)
+    };
     public Monstro(String nome, int pontosDeVida, int forca, int xpConcedido, Arma arma) 
     {
         super(nome, pontosDeVida, forca, arma);
