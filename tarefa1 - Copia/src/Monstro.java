d259cc508a36dc0cdacd28810f97f463b593b67a
Guilherme da Silva Rodrigues <guilherme_silva_rodrigues@outlook.com.br>
2025-09-15 21:16:23 -0300

    teste

---
+++ tarefa1 - Copia/src/Monstro.java
@@ -0,0 +1,25 @@
+public abstract class Monstro extends Personagem 
+{
+    private int xpConcedido;
+    
+    public Monstro(String nome, int pontosDeVida, int forca, int xpConcedido) 
+    {
+        super(nome, pontosDeVida, forca);
+        this.xpConcedido = xpConcedido;
+    }
+
+    @Override
+    public void exibirStatus() 
+    {
+        super.exibirStatus(); 
+        System.out.println("XP concedido: " + xpConcedido);
+        System.out.println(ConsoleColors.BLUE + "===============================================" + ConsoleColors.RESET);
+
+    }
+    
+    public int getXpConcedido() 
+        {
+            return xpConcedido;
+        }
+
+}
