f80dd2c925bea1a5813eba61b405fce5ce3d0659
Giovanni Barbosa Herrero <giovanni.herrero22@gmail.com>
2025-09-25 11:46:13 -0300

    Corrigindo a implementação de Arma.java

--- tarefa3/src/ResistIC/itens/Armas/Arma.java
+++ tarefa3/src/ResistIC/itens/Armas/Arma.java
@@ -1,11 +1,16 @@
 package ResistIC.itens.Armas;
-public abstract class Arma {
+
+import ResistIC.Interfaces.Item;
+
+public abstract class Arma implements Item {
     private int dano;
     private int minNivel;
+    private String nome;
 
     public Arma(int dano, int minNivel) {
         this.dano = dano;
         this.minNivel = minNivel;
+        this.nome = this.getClass().getSimpleName();
     }
 
     public abstract void info();
@@ -17,6 +22,10 @@ public abstract class Arma {
 
     public int getminNivel() {
         return minNivel;
+    }
+
+    public String getNome(){
+        return this.nome;
     }
 
 }
