c5a8ad30e06ede633bc38fcce565d14760d0f085
Giovanni Barbosa Herrero <giovanni.herrero22@gmail.com>
2025-09-28 18:01:51 -0300

    Refatorando a main

--- tarefa4/src/ResistIC/Logica/GameManager.java
+++ tarefa4/src/ResistIC/Logica/GameManager.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 
 import ResistIC.Personagens.Herois.*;
 import ResistIC.Personagens.Monstros.Monstro;
-import ResistIC.itens.Armas.Arma;
-import ResistIC.itens.Armas.Codeblade;
 
 public class GameManager {
 
@@ -18,8 +16,6 @@ public class GameManager {
                     .lerInteiro("Selecione a dificuldade: Pressione 1: FÁCIL\n 2: NORMAL\n 3: DIFÍCIL", 1, 3);
             String string_heroi = InputManager.lerString("Qual heroi você deseja escolher? (Lehilton/Marcelo)");
 
-
-            
             Dificuldade dificuldade_de_jogo;
             switch (dificuldade) {
                 case 1:
@@ -36,7 +32,7 @@ public class GameManager {
             String nome_do_jogador = InputManager.lerString("Digite o nome do seu personagem: ");
             Heroi heroi;
             switch (string_heroi) {
-                case "Lehilton" :
+                case "Lehilton":
                 case "lehilton":
                     heroi = new Lehilton(nome_do_jogador, dificuldade_de_jogo.getVariacaoSorteHeroi());
                     break;
@@ -46,7 +42,6 @@ public class GameManager {
                     break;
             }
 
-            
             ArrayList<FaseDeCombate> fases = gerador_de_fases.gerar(quantidade_de_fases, dificuldade_de_jogo);
 
             for (int i = 0; i < 15; i++) {
@@ -58,7 +53,6 @@ public class GameManager {
                 Thread.sleep(500); // meio segundo
             }
 
-
             // Laço de repetição principal
             for (int i = 0; i < fases.size(); i++) {
                 FaseDeCombate fase_atual = fases.get(i);
@@ -68,32 +62,29 @@ public class GameManager {
                 fase_atual.iniciar(heroi);
 
                 // Laço de combate para cada monstro da fase
-                for (int j = 0; j < fase_atual.getMonstros().length; j++) {
-                    if (heroi.estaVivo()) {
-                        Monstro monstro_atual = fase_atual.getMonstros()[j];
-                        System.out.println(
-                                "O inimigo " + monstro_atual.getNome() + " aparece na arena de combate!");
+                if (heroi.estaVivo()) {
+                    Monstro monstro_atual = fase_atual.getMonstro();
+                    System.out.println("O inimigo " + monstro_atual.getNome() + " aparece na arena de combate!");
 
-                        // Laço while para verificar se os combatentes estão vivos
-                        while (heroi.estaVivo() && monstro_atual.estaVivo()) {
-                            heroi.escolherAcao(monstro_atual).executar(heroi, monstro_atual);
-                            ;
+                    // Laço while para verificar se os combatentes estão vivos
+                    while (heroi.estaVivo() && monstro_atual.estaVivo()) {
+                        heroi.escolherAcao(monstro_atual).executar(heroi, monstro_atual);
 
-                            if (monstro_atual.estaVivo()) {
-                                monstro_atual.escolherAcao(heroi).executar(monstro_atual, heroi);
-                                ;
-                            } else {
-                                heroi.definir_sorte();
-                                if (heroi.getSorte() >= 0.75) {
-                                    heroi.equiparArma(Monstro.largaArma());
-                                }
+                        if (monstro_atual.estaVivo()) {
+                            monstro_atual.escolherAcao(heroi).executar(monstro_atual, heroi);
+                        }
+
+                        else {
+                            heroi.definir_sorte();
+                            if (heroi.getSorte() >= 0.75) {
+                                heroi.equiparArma(Monstro.largaArma());
                             }
                         }
                     }
                 }
-                System.out.println("\n");
-                System.out.println("\n");
             }
+            System.out.println("\n");
+            System.out.println("\n");
 
             System.out.println("Fim do jogo!");
             if (heroi.estaVivo()) {
@@ -101,17 +92,11 @@ public class GameManager {
             } else {
                 System.out.println("Você Falhou na Missão, e todo o IC foi dominado pelas máquinas...");
             }
-        } catch (InterruptedException e) {
+        } catch (
+
+        InterruptedException e) {
             e.printStackTrace();
         }
     }
 
-    static void atualizar(int turno, Heroi heroi, Monstro monstro) {
-        // Atualizar personagens , checar eventos, etc
-
-        heroi.escolherAcao(monstro);
-        if (monstro.estaVivo()) {
-            monstro.escolherAcao(heroi);
-        }
-    }
-};
+}
