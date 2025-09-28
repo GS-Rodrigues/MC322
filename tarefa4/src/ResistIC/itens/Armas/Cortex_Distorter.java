a6a98501c86c393003b2a4435510e220fe4620be
Giovanni Barbosa Herrero <giovanni.herrero22@gmail.com>
2025-09-28 15:37:06 -0300

    Adequando os construtores das novas armas]

--- tarefa4/src/ResistIC/itens/Armas/Cortex_Distorter.java
+++ tarefa4/src/ResistIC/itens/Armas/Cortex_Distorter.java
@@ -1,5 +1,18 @@
 package ResistIC.itens.Armas;
+import ResistIC.Suporte.ConsoleColors;
 
-public class Cortex_Distorter {
-    
+public class Cortex_Distorter extends Arma {
+
+    public Cortex_Distorter() {
+        super(10, 1);
+    }
+
+    public void info() {
+        System.out.println(ConsoleColors.BLUE + "=============== INFO ARMA ===============" + ConsoleColors.RESET);
+        System.out.println(
+                "⚔️ Arma:            " + ConsoleColors.RED + this.getClass().getSimpleName() + ConsoleColors.RESET);
+        System.out.println("❤️ Nivel requerido: " + ConsoleColors.RED + this.getminNivel() + ConsoleColors.RESET);
+        System.out.println("⭐ Dano:            " + ConsoleColors.GREEN + this.getdano() + ConsoleColors.RESET);
+    }
+
 }
