3a605cf976b4a4dfab9b61ab8c52618120f68119
Guilherme da Silva Rodrigues <guilherme_silva_rodrigues@outlook.com.br>
2025-10-12 17:46:16 -0300

    Documentado Interface Item

--- tarefa5/app/src/main/java/ResistIC/Interfaces/GeradorDeFases.java
+++ tarefa5/app/src/main/java/ResistIC/Interfaces/GeradorDeFases.java
@@ -1,12 +1,33 @@
 package ResistIC.Interfaces;
 
-
 import java.util.ArrayList;
 import ResistIC.Logica.Dificuldade;
 import ResistIC.Logica.FaseDeCombate;
+import ResistIC.Personagens.Herois.Heroi;
 
+/**
+ * Representa um objeto capaz de gerar uma sequência de fases para o RPG.
+ *
+ *
+ * <p>
+ * Esta interface pode ser implementada pela classe responsavel por construir os
+ * cenários do RPG.
+ * </p>
+ *
+ * @author Guilherme & Giovani
+ * @version 1.0
+ * @since 2025-10-08
+ */
 public interface GeradorDeFases {
 
+    /**
+     * Gera uma sequencia de fases com uma certa dificuldade selecionada.
+     *
+     * @param quantidadeDeFases quantidade de fases a serem geradas
+     * @param dificuldade       dificuldade das fases a serem geradas.
+     * 
+     * @return uma lista de {@link FaseDeCombate}
+     */
     public ArrayList<FaseDeCombate> gerar(int quantidadeDeFases, Dificuldade dificuldade);
 
 };
--- tarefa5/app/src/main/java/ResistIC/Interfaces/Item.java
+++ tarefa5/app/src/main/java/ResistIC/Interfaces/Item.java
@@ -1,5 +1,31 @@
 package ResistIC.Interfaces;
 
+/**
+ * Representa um item que pode existir no jogo.
+ *
+ * <p>
+ * Um {@code Item} possui um nome que o identifica, podendo ser utilizado em
+ * diferentes contextos do RPG.
+ * </p>
+ *
+ * <p>
+ * Esta interface pode ser implementada por diversos tipos de itens, como armas,
+ * equipamentos, etc
+ * </p>
+ *
+ * @author Guilherme & Giovani
+ * @version 1.0
+ * @since 2025-10-08
+ */
 public interface Item {
+    /**
+     * Retorna o nome do item.
+     *
+     * <p>
+     * O nome identifica o {@code Item} dentro do jogo.
+     * </p>
+     *
+     * @return uma {@code String} contendo o nome do item.
+     */
     String getNome();
 }
