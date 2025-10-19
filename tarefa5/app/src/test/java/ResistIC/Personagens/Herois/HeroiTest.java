74296f070e6f038676e137041bdd23c0332a205e
Guilherme da Silva Rodrigues <guilherme_silva_rodrigues@outlook.com.br>
2025-10-19 09:13:08 -0300

    Refatorado alguns testes

--- tarefa5/app/src/test/java/ResistIC/Personagens/Herois/HeroiTest.java
+++ tarefa5/app/src/test/java/ResistIC/Personagens/Herois/HeroiTest.java
@@ -1,12 +1,50 @@
 package ResistIC.Personagens.Herois;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import ResistIC.Interfaces.Combatente;
+import ResistIC.Personagens.Personagem;
 
 public class HeroiTest {
+
+    private Lehilton heroi;
+    private Lehilton inimigo;
+
+    @BeforeEach
+    void setUp() {
+        heroi = new Lehilton("Lehilton", 0.1);
+        inimigo = new Lehilton("Lehilton", 0.1);
+    }
+
+    @Test
+    void testeReceberDano() {
+        int vidaInicial = inimigo.getVida();
+        inimigo.receberDano(31);
+        assertEquals(vidaInicial - 31, inimigo.getVida(), "Inimigo deve perder 30 de vida");
+    }
+
+    @Test
+    void testeReceberCura() {
+        int vidaInicial = heroi.getVida();
+        heroi.receberCura(1);
+        assertEquals(vidaInicial + 1, heroi.getVida(), "Heroi deve ganhar 1 de vida");
+    }
+
+    @Test
+    void testeEscolherAcao() {
+        inimigo.receberDano(80); // vida <= 30
+        assertEquals("AtaqueBasico",
+                heroi.escolherAcao(inimigo).getClass().getSimpleName(),
+                "Deve escolher AtaqueBasico para inimigo com pouca vida");
+
+        inimigo.receberDano(-50); // vida > 30
+        assertEquals("HabilidadeEspecial",
+                heroi.escolherAcao(inimigo).getClass().getSimpleName(),
+                "Deve escolher HabilidadeEspecial para inimigo com vida alta");
+    }
 
     /**
      * Verifica se Heroi implementa Combatente
@@ -19,4 +57,17 @@ public class HeroiTest {
         assertTrue(implementa,
                 "A classe Heroi deve implementar a interface Combatente");
     }
+
+    /**
+     * Verifica se Heroi herda de Personagem
+     */
+    @Test
+    void deveImplementarInterfacePersonagem() {
+
+        boolean Herda = Personagem.class.isAssignableFrom(Heroi.class);
+
+        assertTrue(Herda,
+                "A classe Heroi deve Herdar da Classe Personagem");
+    }
+
 }
--- tarefa5/app/src/test/java/ResistIC/Personagens/Monstros/MonstroTest.java
+++ tarefa5/app/src/test/java/ResistIC/Personagens/Monstros/MonstroTest.java
@@ -1,12 +1,48 @@
 package ResistIC.Personagens.Monstros;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import ResistIC.Interfaces.Combatente;
+import ResistIC.Interfaces.Lootavel;
+import ResistIC.Personagens.Personagem;
+import ResistIC.Personagens.Herois.Heroi;
 
 public class MonstroTest {
+
+    private Genomex heroi;
+    private Genomex inimigo;
+
+    @BeforeEach
+    void setUp() {
+        heroi = new Genomex();
+        inimigo = new Genomex();
+    }
+
+    @Test
+    void testeReceberDano() {
+        int vidaInicial = inimigo.getVida();
+        inimigo.receberDano(31);
+        assertEquals(vidaInicial - 31, inimigo.getVida(), "Inimigo deve perder 30 de vida");
+    }
+
+    @Test
+    void testeReceberCura() {
+        int vidaInicial = heroi.getVida();
+        heroi.receberCura(1);
+        assertEquals(vidaInicial + 1, heroi.getVida(), "Monstro deve ganhar 1 de vida");
+    }
+
+    @Test
+    void testeEscolherAcao() {
+        inimigo.receberDano(80); // vida <= 30
+        assertEquals("AtaqueBasico",
+                heroi.escolherAcao(inimigo).getClass().getSimpleName(),
+                "Deve sempre escolher AtaqueBasico");
+    }
 
     /**
      * Verifica se Monstro implementa Combatente
@@ -18,5 +54,29 @@ public class MonstroTest {
 
         assertTrue(implementa,
                 "A classe Monstro deve implementar a interface Combatente");
+    }
+
+    /**
+     * Verifica se Monstro implementa Lootavel
+     */
+    @Test
+    void deveImplementarInterfaceLootavel() {
+
+        boolean implementa = Lootavel.class.isAssignableFrom(Monstro.class);
+
+        assertTrue(implementa,
+                "A classe Monstro deve implementar a interface Combatente");
+    }
+
+    /**
+     * Verifica se Monstro herda de Personagem
+     */
+    @Test
+    void deveImplementarInterfacePersonagem() {
+
+        boolean Herda = Personagem.class.isAssignableFrom(Heroi.class);
+
+        assertTrue(Herda,
+                "A classe Monstro deve Herdar da Classe Personagem");
     }
 }
